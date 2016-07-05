a8a7a23a85156d5b52987efcb743aa967b1b075b
Jindrich K. Smitka <jsmitka@smita.info>
2016-07-05 19:52:56 +0200

    Added functional try-except-finally

--- railroad/__init__.py
+++ railroad/__init__.py
@@ -1,10 +1,15 @@
 __author__ = 'Jindrich K. Smitka'
 __email__ = 'smitka.j@gmail.com'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 from .railroad import (  # noqa
     prepare,
     catch,
     get_or_reraise,
     compose,
+)
+from .rescue import (  # noqa
+    reraise,
+    nop,
+    rescue,
 )
